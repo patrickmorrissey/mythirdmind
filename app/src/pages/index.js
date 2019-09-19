78b97785f532fe3e10fab20de49923e83842aac5
Patrick Morrissey <holla@patrickmorrissey.com>
2019-09-19 09:40:25 -0400

    Add CORS proxy URL for rss parsing

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -5,13 +5,38 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Cutup from "../components/cutup"
 
+// Note: some RSS feeds can't be loaded in the browser due to CORS security.
+// To get around this, you can use a proxy.
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
+
+let parser = new Parser();
+
+const rssSources = [
+  "http://feeds.bbci.co.uk/news/rss.xml",
+  "https://hnrss.org/frontpage",
+  "https://lexington.craigslist.org/search/mis?format=rss"
+]
+
+rssSources.forEach(function(index, el) {
+  (async () => {
+ 
+    let feed = await parser.parseURL(CORS_PROXY + index);
+    console.log(feed.title);
+   
+    feed.items.forEach(item => {
+      console.log(item.title + ':' + item.link)
+    });
+   
+  })();
+});
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>My Third Mind</h1>
-    <Cutup feedURL="http://feeds.bbci.co.uk/news/rss.xml" />
-    <Cutup feedURL="https://hnrss.org/frontpage" />
-    <Cutup feedURL="https://lexington.craigslist.org/search/mis?format=rss" />
+    <Cutup feedURL={rssSources[0]} />
+    <Cutup feedURL={rssSources[1]} />
+    <Cutup feedURL={rssSources[2]} />
   </Layout>
 )
 
