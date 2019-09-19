680e51a31d6fdfc3fdcec20f90f1a84e305949a8
Patrick Morrissey <holla@patrickmorrissey.com>
2019-09-19 10:16:01 -0400

    Remove rss-parser and use gatsby-source-rss-feed instead

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `My Third Mind`,
+    description: `Using RSS feeds to generate randomized string cutups.  See William S. Burroughs' and Brion Gysin's "The Third Mind"`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Parser from "rss-parser"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,26 +8,12 @@ import Cutup from "../components/cutup"
 // To get around this, you can use a proxy.
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 
-let parser = new Parser();
-
 const rssSources = [
   "http://feeds.bbci.co.uk/news/rss.xml",
   "https://hnrss.org/frontpage",
   "https://lexington.craigslist.org/search/mis?format=rss"
 ]
 
-rssSources.forEach(function(index, el) {
-  (async () => {
- 
-    let feed = await parser.parseURL(CORS_PROXY + index);
-    console.log(feed.title);
-   
-    feed.items.forEach(item => {
-      console.log(item.title + ':' + item.link)
-    });
-   
-  })();
-});
 
 const IndexPage = () => (
   <Layout>
