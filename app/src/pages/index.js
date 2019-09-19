fe796b6b7719da160889f136caa27ec8a7abe692
Patrick Morrissey <holla@patrickmorrissey.com>
2019-09-19 10:30:18 -0400

    Remove gatsby-source-rss-feed bc you have to declare it on a single feed url

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Parser from "rss-parser"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,12 +9,26 @@ import Cutup from "../components/cutup"
 // To get around this, you can use a proxy.
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 
+let parser = new Parser();
+
 const rssSources = [
   "http://feeds.bbci.co.uk/news/rss.xml",
   "https://hnrss.org/frontpage",
   "https://lexington.craigslist.org/search/mis?format=rss"
 ]
 
+rssSources.forEach(function(index, el) {
+  (async () => {
+ 
+    let feed = await parser.parseURL(CORS_PROXY + index);
+    console.log(feed.title);
+   
+    feed.items.forEach(item => {
+      console.log(item.title + ':' + item.link)
+    });
+   
+  })();
+});
 
 const IndexPage = () => (
   <Layout>
